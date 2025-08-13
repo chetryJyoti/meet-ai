0a2251b4d92f96e617fa87a5f5bcdeaa330005ee
jyoti chetry <jyotichetry087@gmail.com>
2025-08-14 00:22:20 +0530

    typo fixes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 
-import { DashbaordNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
 interface Props {
@@ -11,7 +11,7 @@ const Layout = ({ children }: Props) => {
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-col h-screen w-screen bg-muted">
-        <DashbaordNavbar />
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
