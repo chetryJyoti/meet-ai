d76d8b6d1ac45e67ba328b8298f06389623a2ee8
jyoti chetry <jyotichetry087@gmail.com>
2025-08-15 23:28:14 +0530

    trpc setup

--- src/modules/home/ui/views/home.tsx
+++ src/modules/home/ui/views/home.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
-import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-  if (!session) {
-    return <p>Loading...</p>;
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Chetry" }));
+
   return (
-    <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-    </div>
+    <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>
   );
 };
