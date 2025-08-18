82cbf3858abf5a5296b76614b88df34a02b39397
jyoti chetry <jyotichetry087@gmail.com>
2025-08-18 22:02:22 +0530

    loading and error reusable components

--- src/modules/agents/ui/agent-view.tsx
+++ src/modules/agents/ui/agent-view.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 
@@ -9,9 +11,21 @@ export const AgentsView = () => {
     trpc.agents.getMany.queryOptions()
   );
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <LoadingState
+        title="Loading Agents"
+        description="This may take a few seconds"
+      />
+    );
 
-  if (isError) return <div>Error!</div>;
+  if (isError)
+    return (
+      <ErrorState
+        title="Error Loading Agents"
+        description="Please try again later"
+      />
+    );
 
   return <div>{JSON.stringify(data, null, 2)}</div>;
 };
