c250e7232ba6af3026d042f151700346714f0573
jyoti chetry <jyotichetry087@gmail.com>
2025-08-21 21:49:30 +0530

    agents procedures - protected

---
+++ src/modules/agents/schemas.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const agentsInsertsSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  instructions: z.string().min(1, { message: "Instructions is required" }),
+});
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,15 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  baseProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
+import { agentsInsertsSchema } from "../schemas";
 // import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
+  // todo: change `getMany` to use  `protectedProcedure`
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
 
@@ -12,4 +18,14 @@ export const agentsRouter = createTRPCRouter({
 
     return data;
   }),
+  create: protectedProcedure
+    .input(agentsInsertsSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning();
+
+      return createdAgent;
+    }),
 });
