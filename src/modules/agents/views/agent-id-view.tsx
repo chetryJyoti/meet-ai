4621211bb002f2602831299d362cc1f755680fe2
jyoti chetry <jyotichetry087@gmail.com>
2025-08-25 00:59:31 +0530

    feat: add confirmation dialog for agent removal with associated meetings warning

--- src/modules/agents/views/agent-id-view.tsx
+++ src/modules/agents/views/agent-id-view.tsx
@@ -14,6 +14,7 @@ import { LoadingState } from "@/components/loading-state";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { AgentIdViewHeader } from "../ui/components/agent-id-view-header";
 import { Badge } from "@/components/ui/badge";
+import { useConfirm } from "../hooks/use-confirm";
 
 interface Props {
   agentId: string;
@@ -43,41 +44,55 @@ export const AgentIdView = ({ agentId }: Props) => {
     })
   );
 
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated meetings.`
+  );
+
+  const handleRemoveAgent = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeAgent.mutateAsync({ id: agentId });
+  };
+
   return (
-    <div className="flex-1 p-4 md:px-8 flex flex-col gap-y-4">
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => {}}
-        onRemove={() => {
-          removeAgent.mutate({ id: agentId });
-        }}
-      />
-      <div className="bg-white rounded-lg border">
-        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-          <div className="flex items-center gap-x-3">
-            <GeneratedAvatar
-              variant="bottsNeutral"
-              seed={data.name}
-              className="size-10"
-            />
-            <h2 className="text-2xl font-medium">{data.name}</h2>
-          </div>
-          <Badge
-            variant={"outline"}
-            className="flex items-center gap-x-2 [&>svg]:size-4"
-          >
-            <VideoIcon className="text-blue-700" />
-            {data.meetingCount}{" "}
-            {data.meetingCount === 1 ? "meeting" : "meetings"}
-          </Badge>
-          <div className="flex flex-col gap-y-4">
-            <p className="text-lg font-medium">Instructions</p>
-            <p className="text-neutral-800">{data.instructions}</p>
+    <>
+      <RemoveConfirmation />
+      <div className="flex-1 p-4 md:px-8 flex flex-col gap-y-4">
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => {}}
+          onRemove={handleRemoveAgent}
+        />
+        <div className="bg-white rounded-lg border">
+          <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+            <div className="flex items-center gap-x-3">
+              <GeneratedAvatar
+                variant="bottsNeutral"
+                seed={data.name}
+                className="size-10"
+              />
+              <h2 className="text-2xl font-medium">{data.name}</h2>
+            </div>
+            <Badge
+              variant={"outline"}
+              className="flex items-center gap-x-2 [&>svg]:size-4"
+            >
+              <VideoIcon className="text-blue-700" />
+              {data.meetingCount}{" "}
+              {data.meetingCount === 1 ? "meeting" : "meetings"}
+            </Badge>
+            <div className="flex flex-col gap-y-4">
+              <p className="text-lg font-medium">Instructions</p>
+              <p className="text-neutral-800">{data.instructions}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
