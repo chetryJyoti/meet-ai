3b560aa5a31267b6b973ca1f2ffc7030f7454d7f
jyoti chetry <jyotichetry087@gmail.com>
2025-09-14 14:32:44 +0530

    summary tab done with markdown rendering

--- src/modules/meetings/ui/components/completed-state.tsx
+++ src/modules/meetings/ui/components/completed-state.tsx
@@ -1,3 +1,7 @@
+import Markdown from "react-markdown";
+import Link from "next/link";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+
 import { MeetingGetOne } from "../../types";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -9,6 +13,9 @@ import {
   ClockFadingIcon,
   BookOpenTextIcon,
 } from "lucide-react";
+import { format } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { formatDuration } from "@/lib/utils";
 
 interface Props {
   data: MeetingGetOne;
@@ -65,6 +72,86 @@ export const CompletedState = ({ data }: Props) => {
               className="w-full rounded-lg"
               controls
             />
+          </div>
+        </TabsContent>
+        <TabsContent value="summary">
+          <div className="bg-white rounded-lg border">
+            <div className="px-4 py-5 gap-y-4 flex flex-col col-span-5">
+              <h2 className="text-2xl font-medium capitalize">{data.name}</h2>
+              <div className="flex gap-x-2 items-center">
+                <Link
+                  href={`/agents/${data.agent.id}`}
+                  className="flex items-center gap-x-2 underline underline-offset-4 capitalize"
+                >
+                  <GeneratedAvatar
+                    variant="botttsNeutral"
+                    seed={data.agent.name}
+                    className="size-5"
+                  />
+                  {data.agent.name}
+                </Link>
+                <p>{data.startedAt ? format(data.startedAt, "PPP") : ""}</p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <SparklesIcon className="size-4" />
+                <p>General summary</p>
+              </div>
+              <Badge
+                variant="outline"
+                className="flex items-center gap-x-2 [&>svg]:size-4"
+              >
+                <ClockFadingIcon className="text-blue-700" />
+                {data.duration ? formatDuration(data.duration) : "No duration"}
+              </Badge>
+              <div>
+                <Markdown
+                  components={{
+                    h1: (props) => (
+                      <h1 className="text-2xl font-medium mb-6" {...props} />
+                    ),
+                    h2: (props) => (
+                      <h1 className="text-xl font-medium mb-6" {...props} />
+                    ),
+                    h3: (props) => (
+                      <h1 className="text-lg font-medium mb-6" {...props} />
+                    ),
+                    h4: (props) => (
+                      <h1 className="text-base font-medium mb-6" {...props} />
+                    ),
+                    p: (props) => (
+                      <p className="leading-relaxed mb-6" {...props} />
+                    ),
+                    ul: (props) => (
+                      <ul className="list-disc list-inside mb-6" {...props} />
+                    ),
+                    ol: (props) => (
+                      <ol
+                        className="list-decimal list-inside mb-6"
+                        {...props}
+                      />
+                    ),
+                    li: (props) => <li className="mb-1" {...props} />,
+                    strong: (props) => (
+                      <strong className="font-medium" {...props} />
+                    ),
+                    code: (props) => (
+                      <code
+                        className="bg-gray-100 px-1 py-0.5 rounded"
+                        {...props}
+                      />
+                    ),
+                    blockquote: (props) => (
+                      <blockquote
+                        className="border-l-4 pl-4 italic my-4"
+                        {...props}
+                      />
+                    ),
+                  }}
+                >
+                  {data.summary}
+                </Markdown>
+              </div>
+            </div>
           </div>
         </TabsContent>
       </Tabs>
